57d2843e061a563dc4e95f9ed82a9d78a8030b84
John Jacobsen <john@mail.npxdesigns.com>
2013-03-21 08:48:37 -0700

    Basic MD->HTML conversion done

--- test/cb/core_test.clj
+++ test/cb/core_test.clj
@@ -14,6 +14,28 @@
 
 (def testdir "/tmp/testdir_4_cb")
 
+(defn delete-file-recursively
+  "Delete file f. If it's a directory, recursively delete all its
+contents. Raise an exception if any deletion fails unless silently is
+true. [stolen/modified from clojure-contrib]"
+  [f]
+  (if (.isDirectory f)
+    (doseq [child (.listFiles f)]
+      (delete-file-recursively child)))
+  (.delete f))
+
+(defn splitext [path]
+  (let [parts (.split path "\\.")]
+    (if (= 1 (count parts))
+      [path ""]
+      [(st/join "." (drop-last parts))
+       (last parts)])))
+
+(defn extension [path]
+  (let [no-dir (last (.split path "\\/"))]
+    (if (.contains no-dir ".")
+      (last (.split no-dir "\\.")))))
+
 (defn pathjoin [& args]
   "Join any sequence of directory names, correctly handling duplicate
 '/' characters, including leading '/'."
@@ -25,8 +47,7 @@
       (str "/" base)
       base)))
 
-(defn engine [dir]
-  (fs/mkdir (pathjoin dir "site")))
+(defn path-exists [path] (-> path File. .exists))
 
 (defn files-in [dirname]
   (-> dirname
@@ -34,37 +55,82 @@
       .listFiles
       seq))
 
-(defn remove-files [coll]
-  (println coll) 
-  (map fs/delete coll))
+(comment
+  
+(defn new-filenames-and-extensions
+  [files rootdir]
+  (map (fn [F]
+         (let [path (.getPath F)
+               path-in-markup-dir (st/replace path (pathjoin rootdir "markup/") "")
+               ext (extension path)
+               newext (dest-file-extension ext)
+               newfile-oldext (pathjoin rootdir "site" path-in-markup-dir)
+               [newf _] (splitext newfile-oldext)
+               newfile (str newf "." newext)]
+           [path newfile newext]))
+       files))
 
-(defmacro with-directory [dirname & body]
+)
+
+(defn update-file-path-and-ext [filename sitedir]
+  (let [[basename _] (splitext filename)]
+    (pathjoin sitedir (str basename ".html"))))
+
+(defn engine [args]
+  (let [files (files-in (:markupdir args))]
+    (fs/mkdir (:sitedir args))
+    (doseq [f files]
+      (let [filename (.getName f)
+            target-name (update-file-path-and-ext filename (:sitedir args))
+            markdown-content (slurp f)
+            html-content (md/md-to-html-string markdown-content)]
+        (spit target-name html-content)))))
+
+(defn remove-files [coll]
+  (map #(.delete %) coll))
+
+(defmacro with-tmp-directory [dirname & body]
   `(do
-     (assert (not (path-exists ~dirname)))
+     ;;(assert (not (path-exists ~dirname)))
      (fs/mkdir ~dirname)
      ~@body
-     (remove-files (files-in ~dirname))
-     (fs/delete ~dirname)))
+     (delete-file-recursively (File. ~dirname))))
 
-(defmacro with-file [path body-content & body]
+(defmacro with-tmp-file [path body-content & body]
   `(do
-     (assert (not (path-exists ~path)))
+     ;;(assert (not (path-exists ~path)))
      (spit ~path ~body-content)
      ~@body
      (fs/delete ~path)))
 
 (defmacro with-setup [dir & body]
-  `(with-directory ~dir
+  `(with-tmp-directory ~dir
      (let [markup# (pathjoin ~dir "markup")]
-       (with-directory markup#
+       (with-tmp-directory markup#
          ~@body))))
 
-(defmacro with-engine [dir & body]
+(defmacro with-engine [args & body]
   `(do
-     (engine ~dir)
+     (engine ~args)
      ~@body))
 
-(defn path-exists [path] (-> path File. .exists))
+(try
+  
+(facts "about splitext"
+       (splitext "x.y")      => ["x" "y"]
+       (splitext "x")        => ["x" ""]
+       (splitext "/x/y/z.f") => ["/x/y/z" "f"]
+       (splitext "/x/y/z")   => ["/x/y/z" ""]
+       (splitext "/x/y.z/p.q/r.s.t") => ["/x/y.z/p.q/r.s" "t"])
+
+(facts "about extension"
+       (extension "")        => nil
+       (extension "x")       => nil
+       (extension "x.y")     => "y"
+       (extension "z/x.y")   => "y"
+       (extension "z.b/x.y") => "y"
+       (extension "z.b/x")   => nil)
+
 
 (facts "about pathjoin"
        (pathjoin "a") => "a"
@@ -79,15 +145,23 @@
       (path-exists "/") => truthy)
 
 (fact "I can create a test directory, verify its existence and its removal"
-      (do (with-directory testdir
+      (do (with-tmp-directory testdir
             (path-exists testdir) => truthy)
           (path-exists testdir) => falsey))
 
-(fact "I can place a file in a test directory and verify that file's existence"
-      (with-directory testdir
+(fact "I can place a test file in a test directory, verify that file's
+       existence, as well as its removal"
+      (with-tmp-directory testdir
         (let [tf (pathjoin testdir "foo")]
-          (with-file tf "contents"
-            (path-exists tf) => truthy))))
+          (with-tmp-file tf "contents"
+            (path-exists tf) => truthy)
+          (path-exists tf) => falsey)))
+
+(fact "Putting a file in a test directory does not prevent deletion of the directory"
+      (with-tmp-directory testdir
+        (let [undeleted (pathjoin testdir "foo")]
+          (spit undeleted "contents")))
+      (path-exists testdir) => false)
 
 (fact "Trivial Markdown transformation works"
       (md/md-to-html-string "hi") => "<p>hi</p>")
@@ -98,8 +172,15 @@
           (path-exists (pathjoin testdir "markup")) => truthy)
         (path-exists testdir) => falsey))
 
-;; (fact "running main engine creates target / site directory"
-;;       (with-setup testdir
-;;         (with-engine testdir
-;;           (path-exists (pathjoin testdir "site")) => truthy)))
+(fact "engine processes markup/index.md into site/index.html"
+      (let [markupdir (pathjoin testdir "markup")
+            sitedir (pathjoin testdir "site")
+            testfile (pathjoin markupdir "index.md")
+            outfile (pathjoin sitedir "index.html")]
+        (with-setup testdir
+          (with-tmp-file testfile "hello"
+            (with-engine {:sitedir sitedir, :markupdir markupdir}
+              (path-exists outfile) => truthy)))))
 
+;; NEXT: process all markdown files and make HTML files in target directory
+(catch Exception e (prn e)))
